bc786b7e6dde134aac46ec4d04c639a01a7b7ccf
Maxi <maxiirucci@gmail.com>
2024-11-01 20:16:49 -0300

    fix deploy

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -15,7 +15,7 @@ function Proyects() {
             <img className="rounded-md" src="/visionWeb.png"></img>
             <h1 className="my-5">Landing Page de servicio profesional</h1>
             <p className="p-5">Diseñé una landing page para ofrecer mis servicios como desarrollador web, con un diseño moderno y responsivo. Incluye secciones de "Caracteristicas", "Planes", "Preguntas Frecuentes", además de un acceso rápido a WhatsApp. Esta landing page destaca mis habilidades y facilita la conexión con clientes potenciales, optimizando la conversión de visitas en oportunidades reales.</p>
-            <a href="https://estudiocontablecontable.vercel.app/"><button className="bg-blue-500 p-2 m-5 rounded-md w-40">Ver</button></a>
+            <a href="https://vision-web-sigma.vercel.app/"><button className="bg-blue-500 p-2 m-5 rounded-md w-40">Ver</button></a>
           </div>
           <div className="bg-black rounded-xl p-2 m-10 transition-all transform hover:scale-105 md:my-0">
             <img className="rounded-md" src="/myTasks.png"></img>
@@ -23,7 +23,7 @@ function Proyects() {
             <p className="p-5">
               Este proyecto en Next.js implementa un sistema CRUD para gestión de tareas con registro y autenticación de usuarios, permitiendo crear, gestionar y actualizar tareas propias. Cuenta con una interfaz responsiva, API REST, autenticación con sesiones y tokens, y validación de datos en frontend y backend.
             </p>
-            <a href="#"><button className="bg-blue-500 p-2 m-5 rounded-md w-40">Ver</button></a>
+            <a href="https://my-tasks-three-iota.vercel.app/"><button className="bg-blue-500 p-2 m-5 rounded-md w-40">Ver</button></a>
           </div>
         </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faJs, faNodeJs, faReact, faGitAlt, faPython} from '@fortawesome/free-brands-svg-icons'
 import { TbBrandAstro } from "react-icons/tb";
